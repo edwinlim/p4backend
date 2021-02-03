6e1b73bbf4d47c9fb11c5d51a0c0e4faf6c4f35c
johnlee <johnleerighthere@gmail.com>
2021-02-03 14:27:55 +0800

    creating api for random generator

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -2,46 +2,47 @@ const { response } = require('express')
 const { Sequelize } = require('../models/index')
 const sequelize = require('../models/index')
 const Request = require('../models/request')
+const Tour = require("../models/tour")
 const RequestModel = Request(sequelize.sequelize, sequelize.Sequelize.DataTypes)
-const TourModel = Request(sequelize.sequelize, sequelize.Sequelize.DataTypes)
+const TourModel = Tour(sequelize.sequelize, sequelize.Sequelize.DataTypes)
 
 const controllers = {
-    start: (req, res) => {  
+    start: (req, res) => {
         //to show DB is connected.
 
         RequestModel.findAll()
             .then(response => {
 
-                
+
                 return res.status(200).json(
                     {
                         success: true,
                         request: response
-                        
+
                     }
-                    
+
                 )
-                
+
             })
-      
+
 
     },
 
-    optimize: (req,res) => {
+    optimize: (req, res) => {
 
         // get all delivery requests where status is 'ready to pickup' and 'in wharehouse'. 
         RequestModel.findAll({
             where:
-            
+
                 Sequelize.or(
                     { status: 1 },
                     { status: 3 }
                 )
-            
+
         })
-        .then (response => {
-            console.log(response)
-        })
+            .then(response => {
+                console.log(response)
+            })
 
         // get the latitude and longtitude of the delivery requests of these statuses
 
@@ -55,6 +56,29 @@ const controllers = {
         // insert into tour table
 
 
+
+    },
+
+    generateOtp: (req, res) => {
+
+        // make a function which will generate n number of otp 
+        // generate a 4 digit otp
+
+        // then save it to database in tour_details table where request_id = req.body.jobId
+
+        // TourModel.findAll({
+        //     where:
+
+        //         Sequelize.or(
+        //             { request_id: req.body.jobId }
+        //         )
+
+        // })
+        //     .then(response => {
+        //         response.forEach(x => {
+        //             console.log(x.tour_id)
+        //         })
+        //     })
 
     }
 
--- server.js
+++ server.js
@@ -2,13 +2,22 @@ require('dotenv').config()
 const express = require('express')
 const mainController = require('./controllers/MainController')
 const app = express()
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 5000;
 
 app.use(express.urlencoded({
     extended: true
 }))
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json())
+
 app.get("/", mainController.start)
+
+app.post("/driver", mainController.generateOtp)
 
 
 app.listen(port, () => {
